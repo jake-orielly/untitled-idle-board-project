1ffcbfb22b7c95fcd3fe0defee91507a54702195
Jake O'Rielly <jake.orielly@gmail.com>
2021-05-12 03:49:18 +0000

    Showing army strengths

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -26,6 +26,7 @@
             v-for="(col, colVal) in columns"
             :key="`cell-${colVal}-${rowVal}`"
           >
+            {{parseInt(getArmy(rowVal, colVal))}}
           </td>
         </tr>
       </tbody>
@@ -35,15 +36,32 @@
 
 <script>
 
-class Orc {
+class Unit {
+  static nameSingular= "Unit";
+  static namePlural = "Units";
+  static hp = 1;
+  static attack = 1;
+  static strength = 1;
+
+  static getLevel() {
+    return (this.hp + this.strength + this.attack) / 3;
+  }
+}
+
+class Orc extends Unit {
   static nameSingular = "Orc";
   static namePlural = "Orcs";
+  static hp = 10;
+  static attack = 3;
+  static strength = 6;
+}
 
-  constructor() {
-    this.hp = 10;
-    this.strength = 5;
-    this.attack = 4;
-  }
+class Wolf extends Unit {
+  static nameSingular = "Wolf";
+  static namePlural = "Wolves";
+  static hp = 5;
+  static attack = 3;
+  static strength = 3;
 }
 
 export default {
@@ -59,6 +77,26 @@ export default {
         }
       },
       board:[]
+    }
+  },
+  created() {
+    for (let x = 0; x < this.rows; x++) {
+      this.board.push([])
+      for (let y = 0; y < this.columns; y++)
+        this.board[x][y] = {
+          units: undefined,
+          structures: undefined,
+          terrain: "grass"
+        }
+    }
+    this.board[2][2].units = {
+      type: Wolf,
+      number: 3
+    };
+  },
+  methods: {
+    getArmy(row, col) {
+      return this.board[row][col].units?.type.getLevel();
     }
   }
 }
