8d4edb5143f1b0a6abb4e11655e11c2646c7fb27
Jake O'Rielly <jake.orielly@gmail.com>
2021-06-08 00:21:59 +0000

    Some basic layout

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -1,27 +1,39 @@
 <template>
   <div id="app">
-    <p>{{points}}</p>
-    <table
-      cellspacing="0"
-      class="game-board"
+    <div
+      class="grid-container"
     >
-      <tbody>
-        <tr
-          v-for="(row, rowVal) in rows"
-          :key="`row-${rowVal}`"
+      <div>
+        <p>
+          Points: {{points}}
+        </p>
+        <hr>
+      </div>
+      <div>
+        <table
+          cellspacing="0"
+          class="game-board"
         >
-          <td
-            v-for="(col, colVal) in columns"
-            :key="`cell-${colVal}-${rowVal}`"
-          >
-            <component 
-              :is="'ClickerButton'"
-              @gainPoints="gainPoints"
-            />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          <tbody>
+            <tr
+              v-for="(row, rowVal) in rows"
+              :key="`row-${rowVal}`"
+            >
+              <td
+                v-for="(col, colVal) in columns"
+                :key="`cell-${colVal}-${rowVal}`"
+              >
+                <component 
+                  :is="'ClickerButton'"
+                  @gainPoints="gainPoints"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 <script>
@@ -48,7 +60,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
-table {
+.game-board {
+  margin-top: 2rem;
   border: 2px solid black;
 
   td {
@@ -56,5 +69,18 @@ table {
     width: 2rem;
     height: 2rem;
   }
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 15% auto 15%;
+}
+
+hr {
+  height: 1px;
+  background: black;
+  border:none;
+  width: 80%;
+  margin-left: 0%;
 }
 </style>
