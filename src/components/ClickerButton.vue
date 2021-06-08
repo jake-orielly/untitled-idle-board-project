21e8a85e159c2a5e048e10d3132f63f95af6968d
Jake O'Rielly <jake.orielly@gmail.com>
2021-06-08 00:14:19 +0000

    Pivoting to more classic clicker focus

---
+++ src/components/ClickerButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    @click="clickHandler"
+  >
+    :)
+  </button>
+</template>
+<script>
+
+export default {
+  name: 'ClickerButton',
+  data() {
+    return {}
+  },
+  methods: {
+    clickHandler() {
+      this.$emit('gainPoints',1);
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <table>
-      <tr
-        v-for="troop in troops"
-        :key="troop.class.nameSingular"
-      >
-        <td>
-          {{troop.class.namePlural}}
-        </td>
-        <td>
-          {{troop.number}}
-        </td>
-      </tr>
-    </table>
+    <p>{{points}}</p>
     <table
       cellspacing="0"
       class="game-board"
@@ -26,85 +14,42 @@
             v-for="(col, colVal) in columns"
             :key="`cell-${colVal}-${rowVal}`"
           >
-            {{parseInt(getArmy(rowVal, colVal))}}
+            <component 
+              :is="'ClickerButton'"
+              @gainPoints="gainPoints"
+            />
           </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-
 <script>
-
-class Unit {
-  static nameSingular= "Unit";
-  static namePlural = "Units";
-  static hp = 1;
-  static attack = 1;
-  static strength = 1;
-
-  static getLevel() {
-    return (this.hp + this.strength + this.attack) / 3;
-  }
-}
-
-class Orc extends Unit {
-  static nameSingular = "Orc";
-  static namePlural = "Orcs";
-  static hp = 10;
-  static attack = 3;
-  static strength = 6;
-}
-
-class Wolf extends Unit {
-  static nameSingular = "Wolf";
-  static namePlural = "Wolves";
-  static hp = 5;
-  static attack = 3;
-  static strength = 3;
-}
-
+import ClickerButton from './ClickerButton'
 export default {
   name: 'GameContainer',
+  components: {
+    ClickerButton
+  },
   data() {
     return {
-      rows: 10,
-      columns: 14,
-      troops: {
-        orcs: {
-          number:5,
-          class: Orc
-        }
-      },
-      board:[]
+      rows: 2,
+      columns: 2,
+      board:[],
+      points: 0
     }
-  },
-  created() {
-    for (let x = 0; x < this.rows; x++) {
-      this.board.push([])
-      for (let y = 0; y < this.columns; y++)
-        this.board[x][y] = {
-          units: undefined,
-          structures: undefined,
-          terrain: "grass"
-        }
-    }
-    this.board[2][2].units = {
-      type: Wolf,
-      number: 3
-    };
   },
   methods: {
-    getArmy(row, col) {
-      return this.board[row][col].units?.type.getLevel();
+    gainPoints(val) {
+      this.points += val;
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.game-board {
-  border: 1px solid black;
+table {
+  border: 2px solid black;
 
   td {
     border: 1px solid black;
